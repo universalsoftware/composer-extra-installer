ee275c545888e8036a94bf9b9a7e817ad5dcedfb
Vadim Safonov <VadimSafonov@users.noreply.github.com>
2021-12-28 15:31:35 +0300

    add new methods stubs

--- UnvSoft/ComposerExtraInstaller/Plugin.php
+++ UnvSoft/ComposerExtraInstaller/Plugin.php
@@ -35,6 +35,20 @@ class Plugin implements PluginInterface, EventSubscriberInterface
         $this->composer = $composer;
         $this->io = $io;
     }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
 
     /**
      * {@inheritDoc}
